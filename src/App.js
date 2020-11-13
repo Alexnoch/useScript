f503a713286f2cc49a40f0af2955fe140c3ccb07
Alexnoch <44502017+Alexnoch@users.noreply.github.com>
2020-11-13 16:36:11 +0300

    sdgdfg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './app.css';
+var xhr;
+class App extends Component{
+    constructor(props){
+        super(props)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        this.ajax = this.ajax.bind(this);
+        this.ajaxRes = this.ajaxRes.bind(this);
+
+        this.state ={
+            newP:'Привет'
+        }
+    };
+
+    ajax(){
+        xhr = new XMLHttpRequest();
+        xhr.open("GET","http://alexnoch-blog.ru/button/",true);
+        xhr.send();
+        xhr.addEventListener('readystatechange', this.ajaxRes,false);
+    };
+    ajaxRes(){
+        if(xhr.readyState === 4 && xhr.status === 200){
+            var response = JSON.parse(xhr.responseText);
+            this.setState({
+                newP:response.header
+            });
+        }
+    };
+
+    render(){
+        return(
+        <div className="app_container">
+            <h1>Заголовок нашего приложения</h1>
+            <p>{this.state.newP}</p>
+            <button onClick={this.ajax}>Кнопка</button>
+        </div>
+        )
+    }
 }
-
 export default App;
